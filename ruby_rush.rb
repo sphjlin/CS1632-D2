f553578d2318733fb9b2de0772c052888acb4caa
sphjlin <sphjlin@sina.cn>
2019-03-02 12:37:38 -0500

    Just Started

---
+++ ruby_rush.rb
@@ -0,0 +1,10 @@
+# ruby_rush.rb Sophia Lin
+def message
+  puts'Usage:
+  ruby ruby_rush.rb *seed* *num_prospectors* *num_turns*
+
+  *seed* should be an integer
+  *num_prospectors* should be a non-negative integer
+  *num_turns* should be a non-negative integer'
+  exit(1)
+end
