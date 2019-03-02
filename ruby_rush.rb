e508217029e6310f38aecc2cf998575fec69c24f
sphjlin <sphjlin@sina.cn>
2019-03-02 13:21:49 -0500

    Update ruby_rush.rb

--- ruby_rush.rb
+++ ruby_rush.rb
@@ -1,10 +1,9 @@
 # ruby_rush.rb Sophia Lin
 def message
-  puts'Usage:
-  ruby ruby_rush.rb *seed* *num_prospectors* *num_turns*
-
-  *seed* should be an integer
-  *num_prospectors* should be a non-negative integer
-  *num_turns* should be a non-negative integer'
+  puts "Usage:"
+  puts "ruby ruby_rush.rb *seed* *num_prospectors* *num_turns*"
+  puts "*seed* should be an integer"
+  puts "*num_prospectors* should be a non-negative integer"
+  puts "*num_turns* should be a non-negative integer"
   exit(1)
 end
