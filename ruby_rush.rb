fff4fa776bae037f27d7f2597175573dde7264b6
sphjlin <sphjlin@sina.cn>
2019-03-03 19:05:34 -0500

    Update ruby_rush.rb

--- ruby_rush.rb
+++ ruby_rush.rb
@@ -1,9 +1,5 @@
 # ruby_rush.rb Sophia Lin
-def message
-  puts "Usage:"
-  puts "ruby ruby_rush.rb *seed* *num_prospectors* *num_turns*"
-  puts "*seed* should be an integer"
-  puts "*num_prospectors* should be a non-negative integer"
-  puts "*num_turns* should be a non-negative integer"
-  exit(1)
-end
+require_relative 'simulator.rb'
+
+sim = Simulator.new
+sim.parseargs(ARGV) ? sim.run : exit(1)
