3860b3b4e3b26b160f5962c8ea3a947324b5514c
sphjlin <sphjlin@sina.cn>
2019-03-05 06:27:30 -0500

    Create all_tests.rb

---
+++ all_tests.rb
@@ -0,0 +1,8 @@
+require 'simpleCov'
+SimpleCov.start
+require 'minitest/autorun'
+
+require_relative 'location_test'
+require_relative 'location_relation_test'
+require_relative 'rubyist_test'
+require_relative 'checker_test'
